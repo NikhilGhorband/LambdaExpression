01af6322865867b2b9f0598525de51d0a709c420
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-07-01 03:35:34 +0530

    [Nikhil]Add: Display Student Details

--- Program.cs
+++ Program.cs
@@ -4,28 +4,24 @@
     {
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>();
+            List<Student> studentList = new List<Student>
+        {
+            new Student { Id = 1, Name = "mogu", PhoneNumber = "7896654123", Address = "Kashmir", Age = 23 },
+            new Student { Id = 2, Name = "Adesh", PhoneNumber = "321654987", Address = "Hingoli", Age = 18 },
+            new Student { Id = 3, Name = "Nakul", PhoneNumber = "321654789", Address = "kandhar", Age = 14 },
+            new Student { Id = 4, Name = "Chunchu", PhoneNumber = "789456123", Address = "Sambhajinagar", Age = 16 }
+        };
 
-            students.Add(new Student { Id = 1, Name = "Nikhil", PhoneNumber = "9767158167", Address = "Nanded", Age = 25 });
-            students.Add(new Student { Id = 2, Name = "mogu", PhoneNumber = "7896654123", Address = "Kashmir", Age = 23 });
-            students.Add(new Student { Id = 3, Name = "Adesh", PhoneNumber = "321654987", Address = "Hingoli", Age = 18 });
-            students.Add(new Student { Id = 4, Name = "Nakul", PhoneNumber = "321654789", Address = "kandhar", Age = 14 });
-            students.Add(new Student { Id = 5, Name = "Chunchu", PhoneNumber = "789456123", Address = "Sambhajinagar", Age = 16 });
-
-            
-            
-            // Search for a particular student based on name using lambda expression
-
-            var searchName = "Nikhil" + "";
-            var searchedStudent = students.FirstOrDefault(student => student.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase));
-            if (searchedStudent != null)
+            Console.WriteLine("Student Details:");
+            studentList.ForEach(student =>
             {
-                Console.WriteLine($"\nStudent found - Id: {searchedStudent.Id}, Name: {searchedStudent.Name}, Phone Number: {searchedStudent.PhoneNumber}, Address: {searchedStudent.Address}, Age: {searchedStudent.Age}");
-            }
-            else
-            {
-                Console.WriteLine($"\nStudent with name '{searchName}' not found.");
-            }
+                Console.WriteLine($"ID: {student.Id}");
+                Console.WriteLine($"Name: {student.Name}");
+                Console.WriteLine($"Phone Number: {student.PhoneNumber}");
+                Console.WriteLine($"Address: {student.Address}");
+                Console.WriteLine($"Age: {student.Age}");
+                Console.WriteLine();
+            });
         }
     }
 
